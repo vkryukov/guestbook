a6dca048d2814be68c859017b247454dc4036b92
Victor <victor.kryukov@gmail.com>
2021-09-22 10:52:35 -0700

    Add db tests (p. 16)

--- test/clj/guestbook/db/core_test.clj
+++ test/clj/guestbook/db/core_test.clj
@@ -17,22 +17,14 @@
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
-(deftest test-users
-  (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
-    (is (= 1 (db/create-user!
-              t-conn
-              {:id         "1"
-               :first_name "Sam"
-               :last_name  "Smith"
-               :email      "sam.smith@example.com"
-               :pass       "pass"}
-              {})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"} {})))))
+(deftest test-messages
+  (jdbc/with-transaction
+    [t-conn *db* {:rollback-only true}]
+    (is (= 1 (db/save-message!
+               t-conn
+               {:name "Bob" :message "Hello, world!"}
+               {:connection t-conn})))
+    (is (= {:name "Bob" :message "Hello, world!"}
+           (-> (db/get-messages t-conn {})
+               (first)
+               (select-keys [:name :message]))))))
