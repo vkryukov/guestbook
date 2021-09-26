d466ef7a66c4441a47cf6d3558e052fd21a442f1
Victor <victor.kryukov@gmail.com>
2021-09-26 07:08:56 -0700

    Add message validation

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,21 +1,42 @@
 (ns guestbook.routes.home
   (:require
-   [guestbook.layout :as layout]
-   [guestbook.db.core :as db]
-   [clojure.java.io :as io]
-   [guestbook.middleware :as middleware]
-   [ring.util.response]
-   [ring.util.http-response :as response]))
+    [guestbook.layout :as layout]
+    [guestbook.db.core :as db]
+    [clojure.java.io :as io]
+    [guestbook.middleware :as middleware]
+    [ring.util.response]
+    [ring.util.http-response :as response]
+    [struct.core :as st]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:messages (db/get-messages)}))
+(defn home-page [{:keys [flash] :as request}]
+  (layout/render request "home.html"
+                 (merge
+                   {:messages (db/get-messages)}
+                   (select-keys flash [:name :message :errors]))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
 
+(def message-schema
+  [[:name
+    st/required
+    st/string]
+   [:message
+    st/required
+    st/string
+    {:message  "message must contain at least 10 characters"
+     :validate (fn [msg] (>= (count msg) 10))}]])
+
+(defn validate-message [params]
+  (first (st/validate params message-schema)))
+
 (defn save-message! [{:keys [params]}]
-  (db/save-message! params)
-  (response/found "/"))
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message! params)
+      (response/found "/"))))
 
 (defn home-routes []
   [""
