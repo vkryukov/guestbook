b23188c77750fee62a32594c1d2d464eee929a3b
Victor <victor.kryukov@gmail.com>
2021-10-03 17:49:39 -0700

    P. 86 (book)

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -4,21 +4,34 @@
             [ajax.core :refer [GET POST]]
             [clojure.string :as string]))
 
-(defn send-message! [fields]
+(defn send-message! [fields errors]
   (POST "/message"
-        {:format :json
-         :headers {"Accept" "application/transit+json"
-                   "x-csrf-token" (.-value (.getElementById js/document "token"))}
-         :params @fields
-         :handler #(.log js/console (str "response:" %))
-         :error-handler #(.error js/console (str "error:" %))}))
+        {:format        :json
+         :headers       {"Accept"       "application/transit+json"
+                         "x-csrf-token" (.-value (.getElementById js/document "token"))}
+         :params        @fields
+         :handler       (fn [r]
+                          (.log js/console (str "response:" r))
+                          (reset! errors nil))
+         :error-handler (fn [e]
+                          (.error js/console (str "error:" e))
+                          (reset! errors (-> e :response :errors)))}))
+
+(defn errors-component [errors id]
+  (when-let [error (id @errors)]
+    [:div.notification.is-danger (string/join error)]))
 
 (defn message-form []
-  (let [fields (r/atom {})]
+  (let [fields (r/atom {})
+        errors (r/atom nil)]
     (fn []
       [:div
+       [:p "Name: " (:name @fields)]
+       [:p "Message: " (:message @fields)]
+       [errors-component errors :server-error]
        [:div.field
         [:label.label {:for :name} "Name"]
+        [errors-component errors :name]
         [:input.input
          {:type      :text
           :name      :name
@@ -27,18 +40,16 @@
           :value     (:name @fields)}]]
        [:div.field
         [:label.label {:for :message} "Message"]
+        [errors-component errors :message]
         [:textarea.textarea
          {:name      :message
           :value     (:message @fields)
           :on-change #(swap! fields
                              assoc :message (-> % .-target .-value))}]]
        [:input.button.is-primary
-        {:type :submit
-         :on-click #(send-message! fields)
-         :value "comment"}]
-       [:p "Name: " (:name @fields)]
-       [:p "Message: " (:message @fields)]]
-      )))
+        {:type     :submit
+         :on-click #(send-message! fields errors)
+         :value    "comment"}]])))
 
 
 
