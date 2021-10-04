a025552aba3b21afe9fec8e398fd574110b730b8
Victor <victor.kryukov@gmail.com>
2021-10-03 17:24:56 -0700

    P. 81

--- project.clj
+++ project.clj
@@ -22,11 +22,13 @@
                  [mount "0.1.16"]
                  [nrepl "0.8.3"]
                  [org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.866" :scope "provided"]
                  [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/tools.logging "1.1.0"]
                  [org.webjars.npm/bulma "0.9.1"]
                  [org.webjars.npm/material-icons "0.3.1"]
                  [org.webjars/webjars-locator "0.40"]
+                 [reagent "1.0.0"]
                  [ring-webjars "0.2.0"]
                  [ring/ring-core "1.8.2"]
                  [ring/ring-defaults "0.3.2"]
@@ -36,11 +38,25 @@
   
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
-  :resource-paths ["resources"]
+  :resource-paths ["resources" "target/cljsbuild"]
   :target-path "target/%s/"
   :main ^:skip-aot guestbook.core
 
-  :plugins [] 
+  :plugins [[lein-cljsbuild "1.1.8"]]
+
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+                             :compiler {:output-to "target/cljsbuild/public/js/app.js"
+                                        :output-dir "target/cljsbuild/public/js/out"
+                                        :main "guestbook.core"
+                                        :asset-path "/js/out"
+                                        :optimizations :none
+                                        :source-map true
+                                        :pretty-print true}}}
+              :clean-targets ^{:protect false}
+              [:target-path
+               [:cljsbuild :builds :app :compiler :output-dir]
+               [:cljsbuild :builds :app :compiler :output-to]]
+              }
 
   :profiles
   {:uberjar {:omit-source true
---
+++ src/cljs/guestbook/core.cljs
@@ -0,0 +1,34 @@
+(ns guestbook.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]))
+
+(defn message-form []
+  (let [fields (r/atom {})]
+    (fn []
+      [:div
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [:input.input
+         {:type      :text
+          :name      :name
+          :on-change #(swap! fields
+                             assoc :name (-> % .-target .-value))
+          :value     (:name @fields)}]]
+       [:div.field
+        [:label.label {:for :message} "Message"]
+        [:textarea.textarea
+         {:name      :message
+          :value     (:message @fields)
+          :on-change #(swap! fields
+                             assoc :message (-> % .-target .-value))}]]
+       [:input.button.is-primary
+        {:type :submit :value "comment"}]])))
+
+(defn home []
+  [:div.content>div.columns.is-centered>div.column.is-two-thirds
+   [:div.columns>div.column
+    [message-form]]])
+
+(dom/render
+  [home]
+  (.getElementById js/document "content"))
