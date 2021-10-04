4bc471338e2765aada7d0c987c5affa526564479
Victor <victor.kryukov@gmail.com>
2021-10-03 22:27:35 -0700

    Extract services

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,6 +1,7 @@
 (ns guestbook.routes.home
   (:require
     [guestbook.layout :as layout]
+    [guestbook.messages :as msg]
     [guestbook.db.core :as db]
     [guestbook.middleware :as middleware]
     [ring.util.response]
@@ -14,17 +15,21 @@
   (layout/render request "about.html"))
 
 (defn message-list [_]
-  (response/ok {:messages (vec (db/get-messages))}))
+  (response/ok (msg/message-list)))
 
 (defn save-message! [{:keys [params]}]
-  (if-let [errors (validate-message params)]
-    (response/bad-request {:errors errors})
-    (try
-      (db/save-message! params)
-      (response/ok {:status :ok})
-      (catch Exception e
-        (response/internal-server-error
-          {:errors {:server-error ["Failed to save message!"]}})))))
+  (try
+    (msg/save-message! params)
+    (response/ok {:status :ok})
+    (catch Exception e
+      (let [{id     :guestbook/error-id
+             errors :errors} (ex-data e)]
+        (case id
+          :validation
+          (response/bad-request {:errors errors})
+          ;; else
+          (response/internal-server-error
+            {:errors {:server-error ["Failed to save message!"]}}))))))
 
 (defn home-routes []
   [""
